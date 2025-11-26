b2face596ebeebda7ed6ce3de9377a64db903a55
Ali <alitalaatali30@gmail.com>
2025-11-26 03:24:44 +0200

    update-navbar

--- resources/lang/site.php
+++ resources/lang/site.php
@@ -1,55 +1,56 @@
 <?php
 
 return
-[
-    /**
-     * Navigation bar translations
-     */
-    'nav' => [
-        'get in touch' => 'get in touch',
-    ],
+    [
+        /**
+         * Navigation bar translations
+         */
+        'nav' => [
+            'get in touch' => 'get in touch',
+        ],
 
-    /**
-     * Shared Translations
-     */
-    'shared' => [
-        'Home'                  => 'Home',
-        'About Us'              => 'About Us',
-        'Services'              => 'Services',
-        'Blogs'                 => 'Blogs',
-        'Contact Us'            => 'Contact Us',
-    ],
+        /**
+         * Shared Translations
+         */
+        'shared' => [
+            'Home'                  => 'Home',
+            'About Us'              => 'About Us',
+            'Services'              => 'Services',
+            'Blogs'                 => 'Blogs',
+            'Contact Us'            => 'Contact Us',
+        ],
 
-    /**
-     * Footer Translations
-     */
-    'footer' => [
-        'USEFUL RESOURCES'=>'USEFUL RESOURCES',
-        'Terms & Conditions' => 'Terms & Conditions',
-        'Privacy & Policy' => 'Privacy & Policy',
-        'We are the leaders in the construction and building industries. We are world wide. We never give up on the new challenges. Get in touch now!' => 'We are the leaders in the construction and building industries. We are world wide. We never give up on the new challenges. Get in touch now!',
-        'all rights resevered' => 'all rights resevered',
-        'OUR COMPANY' => 'OUR COMPANY',
-        'our promise to you' => 'our promise to you',
-    ],
+        /**
+         * Footer Translations
+         */
+        'footer' => [
+            'USEFUL RESOURCES' => 'USEFUL RESOURCES',
+            'Terms & Conditions' => 'Terms & Conditions',
+            'Privacy & Policy' => 'Privacy & Policy',
+            'We are the leaders in the construction and building industries. We are world wide. We never give up on the new challenges. Get in touch now!' => 'We are the leaders in the construction and building industries. We are world wide. We never give up on the new challenges. Get in touch now!',
+            'all rights resevered' => 'all rights resevered',
+            'OUR COMPANY' => 'OUR COMPANY',
+            'our promise to you' => 'our promise to you',
+            'Quick Links' => 'Quick Links',
+        ],
 
-    'Read More'=>'Read More',
-    'Contact Us'=>'Contact Us',
-    'Our Services'=>'Our Services',
-    'service_description'=>'service_description',
-    'service_title'=>'service_title',
-    'Sprix'=>'Sprix',
-    'blog_title'=>'blog_title',
-    'Popular FAQs'=>'Popular FAQs',
-    'Frequently Asked Questions'=>'Frequently Asked Questions',
-    'Our Team'=>'Our Team',
-    'Meet Our Experienced Team Members'=>'Meet Our Experienced Team Members',
-    'team_description'=>'team_description',
-    'Testimonial'=>'Testimonial',
-    'What Say Our Clients!'=>'What Say Our Clients!',
-    'Testimonial_description'=>'Testimonial_description',
-    ''
+        'Read More' => 'Read More',
+        'Contact Us' => 'Contact Us',
+        'Our Services' => 'Our Services',
+        'service_description' => 'service_description',
+        'service_title' => 'service_title',
+        'Sprix' => 'Sprix',
+        'blog_title' => 'blog_title',
+        'Popular FAQs' => 'Popular FAQs',
+        'Frequently Asked Questions' => 'Frequently Asked Questions',
+        'Our Team' => 'Our Team',
+        'Meet Our Experienced Team Members' => 'Meet Our Experienced Team Members',
+        'team_description' => 'team_description',
+        'Testimonial' => 'Testimonial',
+        'What Say Our Clients!' => 'What Say Our Clients!',
+        'Testimonial_description' => 'Testimonial_description',
+        ''
 
 
 
-];
+    ];
